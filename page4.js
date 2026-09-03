9f8cb9171c42004ac5da0f464587ac8b0ddaa08c
SoVa <matvey.sliskov@gmail.com>
2026-09-03 15:29:42 +0300

    Update page4.js

--- page4.js
+++ page4.js
@@ -1,4 +1,162 @@
 const doorCards =
+    document.querySelectorAll(
+        ".door-card"
+    );
+
+
+const result =
+    document.getElementById(
+        "result"
+    );
+
+
+const nextButton =
+    document.getElementById(
+        "nextButton"
+    );
+
+
+let opened = false;
+
+
+/* =====================================
+   НАЖАТИЕ НА ДВЕРЬ
+===================================== */
+
+doorCards.forEach(
+    function (card) {
+
+
+        const handle =
+            card.querySelector(
+                ".handle"
+            );
+
+
+        /* Нажатие на ручку */
+
+        handle.addEventListener(
+
+            "click",
+
+            function (event) {
+
+                event.stopPropagation();
+
+                openDoor(card);
+
+            }
+
+        );
+
+
+        /* Можно нажать на саму дверь */
+
+        card.addEventListener(
+
+            "click",
+
+            function () {
+
+                openDoor(card);
+
+            }
+
+        );
+
+
+    }
+);
+
+
+/* =====================================
+   ОТКРЫТИЕ
+===================================== */
+
+function openDoor(selectedDoor) {
+
+
+    /* Если уже открыли */
+
+    if (opened) {
+
+        return;
+
+    }
+
+
+    opened = true;
+
+
+    /* Открываем выбранную */
+
+    selectedDoor.classList.add(
+        "open"
+    );
+
+
+    /* Остальные затемняем */
+
+    doorCards.forEach(
+
+        function (door) {
+
+
+            if (
+                door !== selectedDoor
+            ) {
+
+                door.classList.add(
+                    "disabled"
+                );
+
+            }
+
+
+        }
+
+    );
+
+
+    /* =================================
+       ПОЯВЛЯЕТСЯ ТЕКСТ
+    ================================= */
+
+    setTimeout(
+
+        function () {
+
+            result.classList.add(
+                "visible"
+            );
+
+        },
+
+        1300
+
+    );
+
+
+    /* =================================
+       ПОЯВЛЯЕТСЯ КНОПКА
+    ================================= */
+
+    setTimeout(
+
+        function () {
+
+            nextButton.classList.add(
+                "visible"
+            );
+
+        },
+
+        2200
+
+    );
+
+
+}const doorCards =
     document.querySelectorAll(".door-card");
 
 const result =
